39dc658259ee136d7685631e33ce6ec232fb9c86
molen <molen@LAPTOP-6M3P5BPQ>
2021-10-18 17:07:32 -0500

    actualizacion de flutter sdk y maps api

--- ejem_googlemaps/lib/mimapa.dart
+++ ejem_googlemaps/lib/mimapa.dart
@@ -8,27 +8,38 @@ class MiMapa extends StatefulWidget {
 }
 
 class _MiMapaState extends State<MiMapa> {
-  LatLng? miLatLong = LatLng(19.42847,-99.12766);
+  LatLng? miLatLong = LatLng(19.42847, -99.12766);
   late CameraPosition _cameraPosition;
   late GoogleMapController _controller;
   Set<Marker> _markers = {};
   Location _location = Location();
 
-  void _onMapCreated(GoogleMapController ctrl){
+  void _onMapCreated(GoogleMapController ctrl) {
     _controller = ctrl;
     _location.onLocationChanged.listen((event) {
-      _controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(event.latitude, event.longitude), zoom: 10)
-        )
-      );
+      _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          target: LatLng(event.latitude!, event.longitude!), zoom: 10)));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: CameraPosition(target: miLatLong!),
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              //markers: ,
+              myLocationEnabled: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
